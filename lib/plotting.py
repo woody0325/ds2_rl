928d9746c5338328f675b64eb840f7145dcec736
woody <woody0325@gmail.com>
2021-04-29 18:52:03 +0900

    add plot_two_episode_stats method

--- lib/plotting.py
+++ lib/plotting.py
@@ -96,3 +96,45 @@ def plot_episode_stats(stats, smoothing_window=10, noshow=False):
         plt.show(fig3)
 
     return fig1, fig2, fig3
+
+
+def plot_two_episode_stats(stats1, stats2, smoothing_window=10, noshow=False):
+    # Plot the episode length over time
+    fig1 = plt.figure(figsize=(10,5))
+    plt.plot(stats1.episode_lengths, label='stats1')
+    plt.plot(stats2.episode_lengths, label='stats2')
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Length")
+    plt.title("Episode Length over Time")
+    if noshow:
+        plt.close(fig1)
+    else:
+        plt.show(fig1)
+
+    # Plot the episode reward over time
+    fig2 = plt.figure(figsize=(10,5))
+    rewards_smoothed1 = pd.Series(stats1.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(rewards_smoothed1, label='stats1')
+    rewards_smoothed2 = pd.Series(stats2.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(rewards_smoothed2, label='stats2')
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Reward (Smoothed)")
+    plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
+    if noshow:
+        plt.close(fig2)
+    else:
+        plt.show(fig2)
+
+    # Plot time steps and episode number
+    fig3 = plt.figure(figsize=(10,5))
+    plt.plot(np.cumsum(stats1.episode_lengths), np.arange(len(stats1.episode_lengths)), label='stats1')
+    plt.plot(np.cumsum(stats2.episode_lengths), np.arange(len(stats2.episode_lengths)), label='stats2')
+    plt.xlabel("Time Steps")
+    plt.ylabel("Episode")
+    plt.title("Episode per time step")
+    if noshow:
+        plt.close(fig3)
+    else:
+        plt.show(fig3)
+
+    return fig1, fig2, fig3
